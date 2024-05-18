e0c13f01a7aff58ef72c4f5720dac3b94b9ef58d
JL8255 <j9660628@gmail.com>
2024-05-19 01:03:32 +0200

    feat home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+//import styles from "../style/pages/Home.scss"
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
